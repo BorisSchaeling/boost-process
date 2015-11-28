ea98b6f11d4b3271012c43d2cb58630dfded0303
Nat Goodspeed <nat@lindenlab.com>
2015-11-28 12:46:03 -0500

    Robustify Windows notify_termination GetExitCodeProcess().
    Originally we simply passed in the hProcess obtained by CreateProcess().
    Unfortunately that left the completion callback vulnerable to the business
    logic closing the handle, meaning we could not obtain the child process
    termination code. Duplicating the handle and passing the duplicate solves that
    issue.
    
    Also, the object_handle must survive until its async_wait() call has
    called its completion handler. Otherwise the handler is called too soon
    (immediately!) while the child process is still running.

--- boost/process/windows/initializers/notify_termination.hpp
+++ boost/process/windows/initializers/notify_termination.hpp
@@ -12,6 +12,7 @@
 
 #include <boost/asio/windows/object_handle.hpp>
 #include <boost/process/windows/initializers/initializer_base.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
 
 namespace boost { namespace process { namespace windows { namespace initializers {
@@ -31,28 +32,43 @@ struct notify_termination: public initializer_base
         // On Windows, arrange to wait for child termination *after* execute()
         // returns, since we need the hProcess from the PROCESS_INFORMATION
         // block set by CreateProcessA().
-        asio::windows::object_handle handle(io_service_, e.proc_info.hProcess);
-        // extract native_handle() here to bypass odd object_handle copy behavior
-        asio::windows::object_handle::native_handle_type
-            native_handle(handle.native_handle());
+        // Duplicate the handle so the original handle can be closed
+        // independently. We want identical access; we just want an
+        // independent HANDLE lifespan.
+        HANDLE hProcess;
+        ::DuplicateHandle(GetCurrentProcess(),  // source process
+                          e.proc_info.hProcess, // source handle
+                          GetCurrentProcess(),  // dest process
+                          &hProcess,            // dest handle
+                          0,                    // new access -- ignored
+                          FALSE,                // non-inheritable
+                          DUPLICATE_SAME_ACCESS); // same access as original
+        // Use a shared_ptr to the object_handle object: it's important that
+        // it survive until the child process terminates, and this
+        // on_CreateProcess_success() method will exit momentarily.
+        boost::shared_ptr<asio::windows::object_handle>
+            handle(make_shared<asio::windows::object_handle>(io_service_, hProcess));
         // copy callback_ to avoid
         // error C3480: a lambda capture variable must be from an enclosing function scope
         callback_t callback(callback_);
-        handle.async_wait(
-            [native_handle, callback](const boost::system::error_code&)
+        handle->async_wait(
+            [handle, callback](const boost::system::error_code&)
             {
                 DWORD exit_code;
                 int rc = -1, signal = 0;
-                ::GetExitCodeProcess(native_handle, &exit_code);
-                // High-order bit 0 means voluntary termination. Windows
-                // produces large negative numbers (0xCnnnnnnn) when a process
-                // terminates badly. Pass the same exit_code either way, but
-                // map it to the same (return code, signal) convention we use
-                // for Posix.
-                if (! (exit_code & 0x80000000))
-                    rc = int(exit_code);
-                else
-                    signal = int(exit_code);
+                if (::GetExitCodeProcess(handle->native_handle(), &exit_code))
+                {
+                    // High-order bit 0 means voluntary termination. Windows
+                    // produces large negative numbers (0xCnnnnnnn) when a process
+                    // terminates badly. Pass the same exit_code either way, but
+                    // map it to the same (return code, signal) convention we use
+                    // for Posix.
+                    if (! (exit_code & 0x80000000))
+                        rc = int(exit_code);
+                    else
+                        signal = int(exit_code);
+                }
+                // Whether or not we got exit_code, fire caller's callback.
                 callback(rc, signal);
             });
     }
